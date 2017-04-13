5f16d81e6faea42f313c033dbcf42583031c4a85
OmegaDarkPotato <fdority@ccsuvt.org>
2017-04-13 11:11:04 -0400

    wow
    donny

--- Assets/Scripts/MoveShip.cs
+++ Assets/Scripts/MoveShip.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class MoveShip : MonoBehaviour {
 
 	public float speed = 10f;
-	public float frict = 0.99f;
+	public float frict = 0.01f;
+	private bool applyfrict = true;
 	private Vector3 vel;
 	private Rigidbody rb;
 
@@ -16,31 +17,45 @@ public class MoveShip : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		applyfrict = true;
 		if (Input.GetKey(KeyCode.W)) {
 			vel = vel + transform.forward*speed;
+			applyfrict = false;
 		}
 
 		if (Input.GetKey(KeyCode.S)) {
 			vel = (vel + -transform.forward*speed);
+			applyfrict = false;
 		}
 
 		if (Input.GetKey(KeyCode.A)) {
 			vel = (vel + -transform.right*speed);
+			applyfrict = false;
 		}
 
 		if (Input.GetKey(KeyCode.D)) {
 			vel = vel + transform.right*speed;
+			applyfrict = false;
 		}
 
 		if (Input.GetKey(KeyCode.Space)) {
 			vel = vel + transform.up*speed;
+			applyfrict = false;
 		}
 
 		if (Input.GetKey(KeyCode.LeftControl)) {
 			vel = (vel + -transform.up*speed);
+			applyfrict = false;
 		}
+		//vel = vel * frict;
+		if (applyfrict == true) {
+			rb.drag = (1-frict) * (vel.magnitude/100000);
+			vel = vel * (frict);
+			Debug.Log ("Applied Friction");
 
-		rb.AddForce(vel*Time.deltaTime);
-		vel = vel * frict;
+		}else{
+			rb.drag = 0;
+		}
+		rb.AddForce (vel * Time.deltaTime);
 	}
 }
